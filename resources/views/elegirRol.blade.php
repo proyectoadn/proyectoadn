5106b54f40204a363c2e31c5bb4b4fce7f465600
Daniel <prueba@gmail.com>
2017-01-27 10:04:06 +0100

    Commit

--- resources/views/elegirRol.blade.php
+++ resources/views/elegirRol.blade.php
@@ -6,28 +6,28 @@ Elección de rol
 @endsection
 
 @section('contenido')
+<div class="container">
+    <div class="tituloElegirRol">
+        <h2>Elegir rol de acceso</h2>
+    </div>
+    <div class="row">
+        <form action="" method="POST">
+            {!! csrf_field() !!}
+            <div class="col-md-6 botonRol">
 
-        <div class="tituloElegirRol">
-            <h2>Elegir rol de acceso</h2>
-        </div>
-<div class="row">
-    <form action="" method="POST">
-        {!! csrf_field() !!}
-        <div class="col-md-6 botonRol">
-
-            <button type="submit" name="admin" id="admin" class="botonImagen" value="">
-                <img src="Imagenes/elegirRol/admin.png" class="imagenBoton"/></button>
+                <button type="submit" name="admin" id="admin" class="botonImagen" value="">
+                    <img src="Imagenes/elegirRol/admin.png" class="imagenBoton"/></button>
                 <h4>Administrador</h4>
-        </div>
-    </form>    
-    <form action="" method="POST">
-        <div class="col-md-6 botonRol">
-            <button type="submit" name="usuario" id="usuario" class="botonImagen" value="">
-                <img src="Imagenes/elegirRol/usuario.png" class="imagenBoton"/></button>
+            </div>
+        </form>    
+        <form action="" method="POST">
+            <div class="col-md-6 botonRol">
+                <button type="submit" name="usuario" id="usuario" class="botonImagen" value="">
+                    <img src="Imagenes/elegirRol/usuario.png" class="imagenBoton"/></button>
                 <br>
                 <h4>Usuario</h4>
-        </div>
-    </form>
-</div>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
