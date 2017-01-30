5bb1078a95361606bd1579f05f5084953c72041e
Alber <Alber@Alber-PC>
2017-01-30 10:19:00 +0100

    Tablas Flex hechas

--- resources/views/gestionTareas.blade.php
+++ resources/views/gestionTareas.blade.php
@@ -6,89 +6,85 @@ Gestión de tareas
 @endsection
 
 @section('js')
-    <script>
-        $(document).ready(function() {
-            var usuario = JSON.parse("{{ json_encode($user) }}");
-            alert(usuario);
+<script>
+    $(document).ready(function () {
+        var usuario = JSON.parse("{{ json_encode($user) }}");
+        alert(usuario);
 
-         //   $("#aceptar").on("click",function(){
+        //   $("#aceptar").on("click",function(){
 
-                /*
-                var nom = JSON.stringify($("#usuario").val());
-
-                $.post("servidor.php",{n:nom},
-                        function(respuesta){
-
-
-
-                            if(respuesta === 'Existe'){
-
-                                window.location = "Bienvenido.php";
-                            }
-                            else{
-
-                                window.location = "index.php";
-                            }
-
-                        }).error( function(){
-                    alert("Error");
-                });
-            });
-            */
-        });
-    </script>
+        /*
+         var nom = JSON.stringify($("#usuario").val());
+         
+         $.post("servidor.php",{n:nom},
+         function(respuesta){
+         
+         
+         
+         if(respuesta === 'Existe'){
+         
+         window.location = "Bienvenido.php";
+         }
+         else{
+         
+         window.location = "index.php";
+         }
+         
+         }).error( function(){
+         alert("Error");
+         });
+         });
+         */
+    });
+</script>
 @endsection
 
 @section('contenido')
 
-<div class="row">
-    <div class="contenedorPrincipal">
-        <!--div que contiene los cargos y las categorias-->
-        <div class="cargoCat">
-            <div class='divBotonCargoCat'>
-                <select name="1" size="" class='botonCargoCat form-control'>
-                    <option value="1">Cargo1</option>
-                    <option value="2">Cargo2</option>
-                    <option value="3">Cargo3</option>
-                </select>
-            </div>
-            <div class='divBotonCargoCat'>
-                <select name="cargos" size="" class='botonCargoCat form-control'>
-                    <option value="1">Cargo1</option>
-                    <option value="2">Cargo2</option>
-                    <option value="3">Cargo3</option>
-                </select>
-            </div>
+<div class="contenedorPrincipal">
+    <!--div que contiene los cargos y las categorias-->
+    <div class="cargoCat">
+        <div class='divBotonCargoCat'>
+            <select name="cargos" size="" class='botonCargoCat form-control'>
+                <option value="1">Cargo1</option>
+                <option value="2">Cargo2</option>
+                <option value="3">Cargo3</option>
+            </select>
         </div>
-        <div class='limpiar'></div>
-
-
-        <!--div que contiene todas las columnas-->
-        <div class="contenedorColumnas">
-            <!--1ra columna-->
-            <div class="columna">
-                <p>sgdasdasdgaosuiausgdasdasdgaosuiausgdasdasdgaosuiausgdasdasdgaosudasdgaosuydgauysdgpaiysdgpgdñiaus</p>
-            </div>
-            <!--2da columna-->
-            <div class="columna">
-
-            </div>
-
-            <!--3ra columna-->
-            <div class="columna">
-
-            </div>
-
-            <!--4ta columna-->
-            <div class="columna">
-
-            </div>
-
-            <!--5ta columna-->
-            <div class="columna">
-
-            </div>
+        <div class='divBotonCargoCat'>
+            <select name="categoria" size="" class='botonCargoCat form-control'>
+                <option value="1">Cargo1</option>
+                <option value="2">Cargo2</option>
+                <option value="3">Cargo3</option>
+            </select>
         </div>
+    </div>
+    <div class='limpiar'></div>
+
+
+    <div class="flex-container">
+        <div class="item" id="item1">
+            <div class="dentro"></div>
+            <div class="dentro"></div>
+        </div>
+        <div  class="item" id="item2">
+            2
+        </div>
+        <div  class="item" id="item3">
+            3
+        </div>
+        <div  class="item" id="item4">
+            4
+        </div>
+        <div  class="item" id="item5">
+            5
+        </div>   
+        <div  class="item" id="item4">
+            4
+        </div>
+        <div  class="item" id="item5">
+            5
+        </div>   
     </div>
 </div>
 
