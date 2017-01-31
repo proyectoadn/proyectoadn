dcd136dd846264aba90c7dbf9043d596dc3959ca
Alber <Alber@Alber-PC>
2017-01-31 14:20:00 +0100

    Funciones javascript en archivo externo

--- resources/views/maestra.blade.php
+++ resources/views/maestra.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" type="text/css" href="{!! asset('assets/css/bootstrap.min.css') !!}"/>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/funciones.js') }}"></script>
     <link rel="stylesheet" type="text/css" href="{!! asset('css/estilos.css') !!}"/>
     <link rel="stylesheet" type="text/css" href="{!! asset('css/estiloFlex.css') !!}"/>
     @yield('js')
