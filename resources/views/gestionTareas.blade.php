40fd670ef6701fea6191ecc91bd61fb2249dae49
Daniel <prueba@gmail.com>
2017-02-03 14:12:21 +0100

    Commit actualizado

--- resources/views/gestionTareas.blade.php
+++ resources/views/gestionTareas.blade.php
@@ -103,7 +103,7 @@ Gestión de tareas
 
                         for (var i = 0; i < tarea.length; i++) {
                             if (tarea[i]['estado'] == 1) {
-                                $("#item1").append('<div value="' + tarea[i]['id'] + '" id="hola" class="panel panel-primary tarea" data-toggle="modal" data-target="#myModal"><p>' + tarea[i]['descripcion'] + '</p><p><a href="">' + tarea[i]['modelo'] + '</a></p>\n\
+                                $("#item1").append('<div value="' + tarea[i]['id'] + '" id="hola" class="panel panel-primary tarea"> <p class="textotarea">' + tarea[i]['descripcion'] + '</p><p class="textotarea"><a href="">' + tarea[i]['modelo'] + '</a></p>\n\
                                 <div style="height: 25px; width: 32px; float: right; margin: 0px; padding: 0px; position: relative;">\n\
         <button class="" onclick="popup()" style="width:100%; height:100%; background: transparent; border: 0px; margin:0px;">\n\
 <img alt="Editar tarea" title="Editar tarea" src="Imagenes/editar.png" style="width: 100%; height: 100%;" class=""/></button>\n\
