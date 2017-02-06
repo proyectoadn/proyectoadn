f3ef4ad2dc0a51b8dc202362e0ddd8cf61a2a4c2
Daniel <prueba@gmail.com>
2017-02-06 09:26:57 +0100

    En el controlador y login solucionado fallos

--- app/Http/Middleware/login.php
+++ app/Http/Middleware/login.php
@@ -5,8 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use App\Clases\Usuario;
 
-class login
-{
+class login {
+
     /**
      * Handle an incoming request.
      *
@@ -14,36 +14,44 @@ class login
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
+    public function handle($request, Closure $next) {
 
         $usuario = $request->get('usuario');
         $password = $request->get('password');
 
 
-        $usu = \DB::table('usuario')->where('email','=', $usuario)->get();
+        $usu = \DB::table('usuario')->where('email', '=', $usuario)->get();
 
 
-        if($usu){
-            if(!$usu[0]->confirmado){
-                return redirect('loginconfirm');
-            }
-            else if(\Hash::check($password, $usu[0]->password)){
-                
+        if ($usu) {
+
+
+            if (\Hash::check($password, $usu[0]->password)) {
+
+
                 $usuariosesion = new \Usuario($usu[0]->id_usuario, $usu[0]->nombre, $usu[0]->apellidos, $usu[0]->email, $usu[0]->password);
                 
-                \Session::put('u', $usuariosesion);
 
-                return $next($request);
-            }
-            else{
+                if (!$usu[0]->confirmado) {
+
+                    return redirect('loginconfirm');
+                } 
+                else {
+                    
+                    \Session::put('u', $usuariosesion);
+
+                    return $next($request);
+                }
+            } 
+            else {
 
                 return redirect('loginerror');
             }
-        }
-        else{
+        } 
+        else {
 
             return redirect('loginerror');
         }
     }
+
 }
