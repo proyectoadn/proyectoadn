03609a466e433b3892e3b80857f3a14d77df2151
Daniel <prueba@gmail.com>
2017-02-13 13:44:03 +0100

    Commit para actualziar

---
+++ resources/views/PhpAuxiliares/cabeceraregistro.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<div class="row" style="margin-top: 10px; margin-bottom: 20px;">
+    
+    <div class="col-md-6" style="text-align: center;">
+        
+        <img src="Imagenes/logocentro.png">
+    </div>
+    
+    <div class="col-md-6" style="text-align: center;">
+        
+        <img src="Imagenes/logocentro.png">
+    </div>
+</div>
+
+
+<nav class="navbar navbar-default" role="navigation">
+    <!-- El logotipo y el icono que despliega el menú se agrupan
+         para mostrarlos mejor en los dispositivos móviles -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Desplegar navegación</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="login">Inicio</a>
+    </div>         
+</nav>
