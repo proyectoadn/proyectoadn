4c1f16cf046fbe97d93c596ba677ee84f087f0cd
Alber <Alber@Alber-PC>
2017-02-15 12:32:59 +0100

    Ventana elección de rol, y limpieza de código.2

--- resources/views/Administrar/administrar.blade.php
+++ resources/views/Administrar/administrar.blade.php
@@ -127,10 +127,10 @@ Administracion
                 function (respuesta) {
                     var datos = JSON.parse(respuesta);
                     console.log(datos);
-                    var documento=datos[0]['documento']
-                    var categorias=datos[0]['categorias'];
-                    var entregar=datos[0]['entregar'];
-                    var rol=datos[0]['rol'];
+                    var documento = datos[0]['documento']
+                    var categorias = datos[0]['categorias'];
+                    var entregar = datos[0]['entregar'];
+                    var rol = datos[0]['rol'];
                     if (comentariotexto.length > 0) {
                         mens.push(comentariotexto[0]['mensaje']);
                         mens.push(comentariotexto[0]['descripcion']);
@@ -190,7 +190,7 @@ Administracion
 
                 <div class="row conectardivisores" value="Documentacion" id="item1" style="width: 100%;height: 50px;min-height: 400px;max-height: 400px;">
                     <div class="col-lg-3 col-md-6 divdocumentacion" value=' + documenta'>
-                         <div class="documentacion">
+                        <div class="documentacion">
                             <p>' + documentacion[i]['descripcion'] + '</p>\n\
                             <p class="textotarea"><a href="">' + documentacion[i]['modelo'] + '</a></p>
                             <div class="divisorBotonTarea">
@@ -223,85 +223,59 @@ Administracion
     <div class="modal-dialog anchuraModalCrearDocumentacion">
         <!-- Modal content-->
         <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <!--<div class="modal-header">
                 <div class="form-group">
-                    <input type="text" class="form-control" id="nombreTarea" value="asdad">
+                    <label for="nombreDoc">Nombre Documentación</label>
+                        <input name="nombreDoc" id="nombreDoc" type="text" class="form-control" id="nombreTarea" value="asdad">
                 </div>
-            </div>
+            </div>-->
             <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <div class="row" >
+                    <div class="col-md-12" style="margin-bottom: 10px;">
+                        <label for="nombreDoc">Nombre Documentación</label>
+                        <input name="nombreDoc" id="nombreDoc" type="text" class="form-control" id="nombreTarea" value="asdad">
+                    </div>
                     <div class="col-md-4" style="margin-bottom: 10px;">
                         <h4>Categorias</h4>
                         <div id="categorias" class="checkbox">
-                            <label>
-                                <input type="checkbox" value="">Option 1
+                            <label class="displayBock">
+                                <input type="checkbox" value="">
+                                Esta e
+                            </label>
+                            <label class="displayBock">
+                                <input type="checkbox" value="">
+                                Esta har
+                            </label>
+                            <label class="displayBock">
+                                <input type="checkbox" value="">
+                                Esta 
                             </label>
                         </div>
                     </div>
 
-                    <div  class="col-md-4">
+                    <div id="roles" class="col-md-4">
                         <h4>Roles</h4>
-                        <div id="roles" class="checkbox">
-
-                            <label>
-                                <input type="checkbox" value="">Option 1
-                            </label>
-                        </div>
+                        <select class="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                        </select>
                     </div>
 
                     <div id="entregar" class="col-md-4">
                         <h4>Entrega</h4>
-                        <div id="entregar" class="checkbox">
+                        <select class="form-control">
+                            <option>1</option>
+                        </select>
 
-                            <div class="col-md-6">
-                                <label>
-                                    <input type="checkbox" value="">Option 1
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 1
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 1
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 1
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 1
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 1
-                                </label>
-                            </div>
-                            <div class="col-md-6">
-                                <label>
-                                    <input type="checkbox" value="">Option 12
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 12
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 12
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 12
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 12
-                                </label>
-                                <label>
-                                    <input type="checkbox" value="">Option 12
-                                </label>
-                            </div>
-                        </div>
                     </div>
                     <div class="col-md-12">
-                        <h3>Nombre de la documentación</h3>
-                        <textarea id="textocomentario" name="mensaje" class="form-control" maxlength="200" rows="3" type="text" 
-                                  style="width: 100%; height: 60%;; margin-bottom:10px; resize: none;"
-                                  value="NUMERO MDF3234"></textarea>
-                        <div id="contador" class="text-right text-danger" style="font-size:0.8em;"></div>
+                        <label for="nombreModelo">Modelo</label>
+                        <input name="nombreModelo" id="nombreModelo" type="text" class="form-control" id="nombreTarea" value="asdad">
+                        <br>
+
+                        <label for="linkModelo">Link del modelo</label>
+                        <input name="linkModelo" id="linkModelo" type="text" class="form-control" id="nombreTarea" value="asdad">
                     </div>
                 </div>
             </div>
