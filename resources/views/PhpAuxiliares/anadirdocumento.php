80531ee664c416db38d4c13be189e4539d10c7b6
Nazario <Lehonartcruz@mail.com>
2017-02-17 14:16:55 +0100

    Añadir documentacion

---
+++ resources/views/PhpAuxiliares/anadirdocumento.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DAW2
+ * Date: 31/01/2017
+ * Time: 10:42
+ */
+
+require_once 'Conexion.php';
+
+
+$conexion = new Conexion();
+$vector=[];
+$datos = json_decode($_POST['datos']);
+
+$descripcion=utf8_decode($datos[0]);
+if(count($datos[1])==1){
+    $categoria[]=$datos[1];
+}
+else{
+    for($i=0;$i<count($datos[1]);$i++){
+        $categoria[]=$datos[1][$i];
+    }
+}
+
+$rol=$datos[2];
+$entrega=$datos[3];
+$modelo=$datos[4];
+
+if($conexion->conectar()){
+    for($i=0;$i<count($categoria);$i++){
+        $conexion->insertar_documento($descripcion,$categoria[$i],$rol,$entrega,$modelo);
+    }
+
+    $aux="ok";
+}
+
+$conexion->cerrar_Conexion2();
+
+$vector=  json_encode($aux);
+echo $vector;
