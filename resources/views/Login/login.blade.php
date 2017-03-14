19c216a6ca97bed04736dbd28baebc33456fa909
Daniel <prueba@gmail.com>
2017-03-14 14:11:14 +0100

    Logo en la pagina de login

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -1,77 +1,83 @@
 @extends('../maestra')
 
 @section('titulo')
-    Login
+Login
 @endsection
 
 
 @section('contenido')
 
-    <script src="jquery-2.1.4.js"></script>
-    <script src="jquery-ui.min.js"></script>
+<script src="jquery-2.1.4.js"></script>
+<script src="jquery-ui.min.js"></script>
 
 
-    <div class="container">
-        <div class="panel panel-primary login">
-            <div class="panel-body">
-                <h2 class="form-signin-heading">Iniciar sesión</h2>
-            </div>
-            <div class="panel-footer">
-                <form action="validar" method="POST">
-                    {!! csrf_field() !!}
+<div class="container">
 
-                    <!--Email-->
-                    <label for="inputEmail" value="Usuario">Usuario</label>
-                    <div class="input-group" style="margin-bottom: 5px;">
-                        <span class="input-group-addon">@</span>
-                        <input type="text" name="usuario" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
-                    </div>
-                    <!--Contraseña-->
-                    <label for="inputPassword" value="Password">Contraseña</label>
-                    <div class="input-group" style="margin-bottom: 5px;">
-                        <span class="input-group-addon" id="span1"><i class="glyphicon glyphicon-lock" id="codificarDecodificar"></i></span>
-                        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
-                    </div>
+    <div class="logopaginalogin">
+        
+        <a href="login"><img src="Imagenes/Logos/logoGETY.png"></a>
+    </div>
 
-                    <a href="enviarpassword">¿Has olvidado la contraseña?</a>
-                    <br>
-
-                    <br>
-
-                    <input class="btn btn-lg btn-primary btn-block" type="submit" value="Entrar">
-
-                    <br>
-                </form>
-
-                <form action="registro" method="POST">
-                    {!! csrf_field() !!}
-
-                    <input class="btn btn-lg btn-primary btn-block" type="submit" value="Registro">
-                </form>
-
-            </div>
+    <div class="panel panel-primary login">
+        <div class="panel-body">
+            <h2 class="form-signin-heading">Iniciar sesión</h2>
         </div>
+        <div class="panel-footer">
+            <form action="validar" method="POST">
+                {!! csrf_field() !!}
 
-        <script>
+                <!--Email-->
+                <label for="inputEmail" value="Usuario">Usuario</label>
+                <div class="input-group" style="margin-bottom: 5px;">
+                    <span class="input-group-addon">@</span>
+                    <input type="text" name="usuario" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+                </div>
+                <!--Contraseña-->
+                <label for="inputPassword" value="Password">Contraseña</label>
+                <div class="input-group" style="margin-bottom: 5px;">
+                    <span class="input-group-addon" id="span1"><i class="glyphicon glyphicon-lock" id="codificarDecodificar"></i></span>
+                    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                </div>
 
-            $(function () {
+                <a href="enviarpassword">¿Has olvidado la contraseña?</a>
+                <br>
+
+                <br>
+
+                <input class="btn btn-lg btn-primary btn-block" type="submit" value="Entrar">
+
+                <br>
+            </form>
+
+            <form action="registro" method="POST">
+                {!! csrf_field() !!}
+
+                <input class="btn btn-lg btn-primary btn-block" type="submit" value="Registro">
+            </form>
+
+        </div>
+    </div>
+
+    <script>
+
+        $(function () {
 
 
-                $("#datos").on("click", function () {
+            $("#datos").on("click", function () {
 
-                    $("#meterdatos").append('<p>Some text in the modal.</p>');
-                    $("#meterdatos").append('<p>Some text in the modal.</p>');
-                    $("#meterdatos").append('<button type="button" id="prueba" value="Prueba de boton" class="btn btn-info btn-lg">Boton</button>');
+                $("#meterdatos").append('<p>Some text in the modal.</p>');
+                $("#meterdatos").append('<p>Some text in the modal.</p>');
+                $("#meterdatos").append('<button type="button" id="prueba" value="Prueba de boton" class="btn btn-info btn-lg">Boton</button>');
 
-                    $("#prueba").on("click", function () {
+                $("#prueba").on("click", function () {
 
-                        alert($(this).val());
-                    });
+                    alert($(this).val());
                 });
             });
-        </script>
+        });
+    </script>
 
-    </div>
+</div>
 @endsection
 
 @section('footer')
