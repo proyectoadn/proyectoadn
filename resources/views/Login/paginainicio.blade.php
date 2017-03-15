d8d254854b50c7de52f034feaa957310ed139815
Daniel <prueba@gmail.com>
2017-03-15 09:25:52 +0100

    Pagina de inicio de la aplicacion

---
+++ resources/views/Login/paginainicio.blade.php
@@ -0,0 +1,49 @@
+@extends('../maestra')
+
+@section('titulo')
+Gestión de tareas
+@endsection
+
+
+@include ('PhpAuxiliares/cabecera')
+
+
+@section('contenido')
+
+
+<div class="divpaginainicio">
+
+    <div class="row">
+
+        <div class="col-md-6">
+
+
+        </div>
+        
+        <div class="col-md-6 divinformacionpaginainicio">
+            
+            <img src="Imagenes/Logos/logoGETY.png">
+            
+            <p>
+                Descripcion de la aplicacion.
+            </p>
+            
+            <form action="seguir" method="POST">
+                {!! csrf_field() !!}
+                
+                <button type="submit" name="seguir" class="btn btn-primary btn-lg">Seguir con la aplicacion <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+            </form>
+        </div>
+
+    </div>
+
+</div>
+
+@endsection
+
+
+@section('footer')
+
+@include ('PhpAuxiliares/footer')
+
+@endsection
